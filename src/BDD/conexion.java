ab2897e3f386a4d0e7af90c8b0116876e5da2c5d
Juliet Ortunio <julsortuno@gmail.com>
2025-01-09 00:22:34 -0500

    FEAT:conexion_bdd-frame_de_acceso

---
+++ src/BDD/conexion.java
@@ -0,0 +1,28 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package BDD;
+import java.sql.*;
+/**
+ *
+ * @author Usuario
+ */
+public class conexion {
+    private static final String URL = "jdbc:mysql://localhost:3306/mora";
+    private static final String USER = "root";
+    private static final String PASS = "";
+    
+    public static Connection conectar()
+    {
+        Connection con = null;
+        try {
+            con = DriverManager.getConnection(URL, USER, PASS);
+            System.out.println("Conexión exitosa");
+        } catch (SQLException e) {
+            System.err.println("Error al conectar con la base de datos: " + e.getMessage());
+        }
+        return con;
+    }
+    
+}
