e7196c49cb68110b351d6825ae0ac8f9bbd445aa
Andres Chavez <eliceojimenez1995@gmail.com>
2025-01-18 16:12:57 -0500

    feat:MoraS.A.

---
+++ Documents/PracticaHome/CRUD/CRUD/src/main/java/com/Tienda/CRUD/controller/AdministradorController.java
@@ -0,0 +1,31 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.Tienda.CRUD.controller;
+
+import com.Tienda.CRUD.model.Producto;
+import com.Tienda.CRUD.service.ProductoService;
+import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+
+@Controller
+@RequestMapping("/administrador")
+public class AdministradorController {
+    
+    @Autowired
+    private ProductoService productoService;
+
+    @GetMapping("")
+    public String home(Model model) {
+        List<Producto> productos = productoService.findAll();
+        model.addAttribute("productos", productos);
+        return "administrador/home";
+    }
+
+}
---
+++ Documents/PracticaHome/CRUD/CRUD/src/main/java/com/Tienda/CRUD/model/Usuario.java
@@ -0,0 +1,130 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.Tienda.CRUD.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.Table;
+import java.util.List;
+
+/**
+ *
+ * @author Usuario
+ */
+@Entity
+@Table(name = "usuarios")
+public class Usuario {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+    private String nombre;
+    private String username;
+    private String correo;
+    private String direccion;
+    private String contrasenia;
+    private String tipo;
+    
+    @OneToMany(mappedBy="usuario")
+    private List<Producto> productos;
+    @OneToMany(mappedBy="usuario")
+    private List<Orden> ordenes;
+
+    public Usuario(Integer id, String nombre, String username, String correo, String direccion, String contrasenia, String tipo) {
+        this.id = id;
+        this.nombre = nombre;
+        this.username = username;
+        this.correo = correo;
+        this.direccion = direccion;
+        this.contrasenia = contrasenia;
+        this.tipo = tipo;
+    }
+
+    public Usuario() {
+    }
+    
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getCorreo() {
+        return correo;
+    }
+
+    public void setCorreo(String correo) {
+        this.correo = correo;
+    }
+
+    public String getDireccion() {
+        return direccion;
+    }
+
+    public void setDireccion(String direccion) {
+        this.direccion = direccion;
+    }
+
+    public String getContrasenia() {
+        return contrasenia;
+    }
+
+    public void setContrasenia(String contrasenia) {
+        this.contrasenia = contrasenia;
+    }
+
+    public String getTipo() {
+        return tipo;
+    }
+
+    public void setTipo(String tipo) {
+        this.tipo = tipo;
+    }
+
+    public List<Producto> getProductos() {
+        return productos;
+    }
+
+    public void setProductos(List<Producto> productos) {
+        this.productos = productos;
+    }
+
+    public List<Orden> getOrdenes() {
+        return ordenes;
+    }
+
+    public void setOrdenes(List<Orden> ordenes) {
+        this.ordenes = ordenes;
+    }
+
+    @Override
+    public String toString() {
+        return "Usuario{" + "id=" + id + ", nombre=" + nombre + ", username=" + username + ", correo=" + correo + ", direccion=" + direccion + ", contrasenia=" + contrasenia + ", tipo=" + tipo + ", productos=" + productos + ", ordenes=" + ordenes + '}';
+    }
+
+    
+    
+}
